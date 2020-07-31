df300f132a81ce97a348fef818de813e9bd46c3d
Manish Kashyap <groddmanish@gmail.com>
2020-07-31 17:08:38 +0530

    changes

---
+++ client/build/precache-manifest.12be57b407d651610bb6ec6df20b26cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ebb545ce8363653b7f8bd1649674eed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "289d5a43becb40c860cf",
+    "url": "/static/css/main.7b5857a1.chunk.css"
+  },
+  {
+    "revision": "7b52eddcea162a110ffc",
+    "url": "/static/js/2.781d8bbf.chunk.js"
+  },
+  {
+    "revision": "289d5a43becb40c860cf",
+    "url": "/static/js/main.6cfe47ff.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+    mongoURI: "mongodb+srv://grodd:SG0JmVIx3PCrXXlf@cluster0.jyefp.mongodb.net/switchon?retryWrites=true&w=majority",
+    secretOrKey: "secret",
+    accessKeyId: "AKIAIJESNZ3FTMQN7FBA",
+    secretAccessKey: "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"
+}
